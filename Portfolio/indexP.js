1c0cd07464759fb5641fd5fb35bc479c4795b8d8
charancandie <122959973+charancandie@users.noreply.github.com>
2023-04-15 21:09:03 +0530

    Add files via upload

---
+++ Portfolio/indexP.js
@@ -0,0 +1,24 @@
+var a = 20;
+console.log(a); //Print integer value of the variable
+
+var b = 20;
+console.log(typeof(b)); //Print type of the variable
+
+var c = 20;
+var c = typeof(c);
+console.log(c); //Print type of the variable in another method
+
+var d = '10'; //We can also use double quotes "" 
+console.log(d); //Print string value of the variable
+
+var e = 5;
+console.log(d-e);
+
+// type of operators + - * / %
+
+var f = 4;
+var g = 3;
+
+var h = f**g //multiplying 4 into 3 times  
+
+console.log(h);
